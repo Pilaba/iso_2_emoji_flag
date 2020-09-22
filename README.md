b09e20cab014e5cfc91578b5a36c27086e040c55
pilaba <pilaba@live.com>
2020-09-22 14:09:14 -0500

    primer commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# iso_2_emoji_flag
+
+Small package to obtain an emoji from his respective ISO code, and visceversa.
+
+## Getting Started
+
---
+++ lib/iso_2_emoji_flag.dart
@@ -0,0 +1,7 @@
+library iso_2_emoji_flag;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
