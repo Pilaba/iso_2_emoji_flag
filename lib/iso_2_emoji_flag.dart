358c25a1379c58b92ac882d5a4c5d4f634b752cb
ING. S.C. Rigoberto Alejandres García <isc.alej@gmail.com>
2020-11-19 19:12:59 -0600

    Apply dartfmt

--- lib/iso_2_emoji_flag.dart
+++ lib/iso_2_emoji_flag.dart
@@ -1,9 +1,13 @@
 library iso_2_emoji_flag;
 
-String iso2EmojiFlag(String iso){
-  return iso.toUpperCase().split("").map((e) => String.fromCharCode(e.codeUnitAt(0) + 127397)).join("");
+String iso2EmojiFlag(String iso) {
+  return iso
+      .toUpperCase()
+      .split("")
+      .map((e) => String.fromCharCode(e.codeUnitAt(0) + 127397))
+      .join("");
 }
 
-String emojiFlag2Iso(String flag){
+String emojiFlag2Iso(String flag) {
   return flag.runes.map((rune) => String.fromCharCode(rune - 127397)).join("");
 }
